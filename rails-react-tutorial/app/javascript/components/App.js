f8cfaf48a0a5f06c0887ae4d2316e18c9fa42a53
Andrew Hampson <andrew.hampson@itison.com>
2020-05-17 08:04:54 +0100

    bugs ironed out, back to working app... Step 9.

--- rails-react-tutorial/app/javascript/components/App.js
+++ rails-react-tutorial/app/javascript/components/App.js
@@ -3,7 +3,12 @@ import PropTypes from "prop-types"
 
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
+import { Provider } from 'react-redux'
+
 import HelloWorld from './HelloWorld'
+
+import configureStore from '../configureStore'
+const store = configureStore();
 
 class App extends React.Component {
   render () {
