c7a00e9caa065b8a2f2169c1c681659d20adf4c3
Andrew Hampson <andrew.hampson@itison.com>
2020-05-19 06:54:37 +0100

    Final state - ish

--- rails-react-tutorial/app/javascript/components/HelloWorld.js
+++ rails-react-tutorial/app/javascript/components/HelloWorld.js
@@ -3,11 +3,23 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST';
+const GET_THINGS_SUCCESS = 'GET_THINGS_SUCCESS';
 
 function getThings() {
   console.log('getThings() Action!!')
+  return dispatch => {
+    dispatch({ type: GET_THINGS_REQUEST });
+    return fetch('v1/things.json')
+      .then(response => response.json())
+      .then(json => dispatch(getThingsSuccess(json)))
+      .catch(error => console.log(error));
+  };
+};
+
+export function getThingsSuccess(json) {
   return {
-    type: GET_THINGS_REQUEST
+    type: GET_THINGS_SUCCESS,
+    json
   };
 };
 
@@ -21,7 +33,7 @@ class HelloWorld extends React.Component {
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}
-        <button className="getThingsBtn" onClick={() => this.props.getThings()}>getThings</button>
+        <button className="getTningsBtn" onClick={() => this.props.getThings()}>getThings</button>
         <br />
         <ul>{ thingsList }</ul>
       </React.Fragment>
@@ -36,3 +48,4 @@ const structuredSelector = createStructuredSelector({
 const mapDispatchToProps = { getThings };
 
 export default connect(structuredSelector, mapDispatchToProps)(HelloWorld);
+
